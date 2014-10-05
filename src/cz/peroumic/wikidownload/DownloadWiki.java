c310f4bfdb4472d3f8e67a1b3c5d289c92b4799f
tulinkry <k.tulinger@seznam.cz>
2014-10-05 03:54:59 +0200

    merge files

--- src/cz/peroumic/wikidownload/DownloadWiki.java
+++ src/cz/peroumic/wikidownload/DownloadWiki.java
@@ -14,13 +14,18 @@ import java.util.List;
 public class DownloadWiki {
     protected int threads;
     protected String outputFolderPath;
-    protected BufferedReader file;
+    protected BufferedReader localFile;
     protected long counter = 0;
+    protected long bytes = 0;
+    protected long readBytes = 0;
+    protected String language = "cs";
+
 
     public DownloadWiki(int threads, String inputFilePath,String language) throws FileNotFoundException {
         this.threads = threads;
-        this.file = new BufferedReader(new FileReader(inputFilePath));
+        this.localFile = new BufferedReader(new FileReader(inputFilePath));
         File ff = new File(inputFilePath);
+        this.bytes = ff.length();
         outputFolderPath = ff.getAbsolutePath();
         outputFolderPath = outputFolderPath.substring(0,outputFolderPath.lastIndexOf('\\'));
         File newFolder = new File(outputFolderPath+"\\"+language);
@@ -28,6 +33,7 @@ public class DownloadWiki {
             newFolder.mkdir();
         }
         outputFolderPath = newFolder.getPath();
+        this.language = language;
     }
 
     public String getOutputFolderPath(){
@@ -36,22 +42,46 @@ public class DownloadWiki {
 
 
     public synchronized List<String> getLinks(int i) throws IOException {
-        String line = file.readLine();
-        List<String> list = new ArrayList<>();
+        String line = localFile.readLine();
+        this.readBytes += line.getBytes("UTF-8").length;
+        List<String> list = new ArrayList<String>();
         int y = 0;
         while (line != null && i != y) {
             list.add(line);
-            line = file.readLine();
+            line = localFile.readLine();
+            this.readBytes += line.getBytes("UTF-8").length;
             y++;
         }
         if (list.size() == 0) {
             return null;
         }
         counter+=list.size();
-        System.out.println(counter);
+        //System.out.println(counter);
+        System.out.format("%d%% [%d/%d]\r\n", (this.readBytes / this.bytes), this.readBytes, this.bytes);
         return list;
     }
 
+    private void mergeFiles() throws IOException
+    {
+        File finalFile = new File ( getOutputFolderPath() + "\\" + this . language + ".txt" );
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(finalFile, true);
+            for (int i = 0; i < threads; i++) {
+                File localFile = new File(getOutputFolderPath() + "\\" + "T" + i + ".txt");
+                FileInputStream fis = new FileInputStream(localFile);
+                byte[] buffer = new byte[8192];
+                int count;
+                while ((count = fis.read(buffer)) > 0)
+                    fos.write(buffer, 0, count);
+                fos.flush();
+                fis.close();
+                localFile . delete ();
+            }
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+    }
 
     public void start() throws IOException {
         try {
@@ -68,7 +98,8 @@ public class DownloadWiki {
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            file.close();
+            localFile.close();
         }
+        mergeFiles();
     }
 }
